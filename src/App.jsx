f1da08bd710cf8e690f7096b8195b6bb4a5f6c51
wassila <rzgwassila@gmail.com>
2025-03-11 01:48:16 +0100

    add files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Box from "./components/box";
+function App() {
+  return (
+    <>
+      <Box />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/box.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./box.css";
+
+export default function Box() {
+  const [tasks, setTasks] = useState([
+    "Algo",
+    "Analyse",
+    "Algebre",
+    "Physique",
+    "Structure",
+  ]);
+  const [task, setTask] = useState("");
+
+  // Function to add a new task
+  const addTask = () => {
+    if (task.trim() !== "") {
+      setTasks([...tasks, task]); // Add task to list
+      setTask(""); // Clear input field
+    }
+  };
+
+  return (
+    <div className="box">
+      <div className="header">
+        <input
+          type="text"
+          placeholder="Enter your Task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+
+      <div className="list">
+        <ul>
+          {tasks.map((t, index) => (
+            <li key={index}>
+              <p>{t}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
